ba6c8656f8a9a6476f30371eee837a7ceb203565
AlbertoJTD <albertojtd@outlook.com>
2023-05-29 10:58:11 -0600

    Add cuenta model

---
+++ ManejoPresupuesto/Models/CuentaCreacionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Models
+{
+	public class CuentaCreacionViewModel: Cuenta
+	{
+		// IEnumerable => Coleccion de elementos
+		// SelectListItem => Permite crear select tag
+		public IEnumerable<SelectListItem> TiposCuentas { get; set; }
+    }
+}
