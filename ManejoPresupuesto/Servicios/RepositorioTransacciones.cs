657b965987e970827b10737a42386972289fcb65
AlbertoJTD <albertojtd@outlook.com>
2023-05-30 15:04:50 -0600

    Create Transacciones

--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -21,7 +21,7 @@ namespace ManejoPresupuesto.Servicios
 		public async Task Crear(Transaccion transaccion)
 		{
 			using var connection = new SqlConnection(connectionString);
-			var id = await connection.QuerySingleAsync<int>(@"Transacciones_Insertar;",
+			var id = await connection.QuerySingleAsync<int>("Transacciones_Insertar",
 							new {transaccion.UsuarioId, transaccion.FechaTransaccion, transaccion.Monto,
 								transaccion.CategoriaId, transaccion.CuentaId, transaccion.Nota}, commandType: System.Data.CommandType.StoredProcedure);
 			transaccion.Id = id;
