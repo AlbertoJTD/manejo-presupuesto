cc6a6e9045b22885c892f8507d79d531da5f2ea3
AlbertoJTD <albertojtd@outlook.com>
2023-05-30 12:45:13 -0600

    Add form for Transaccion

---
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -0,0 +1,41 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+	public class TransaccionesController : Controller
+	{
+		private readonly IServicioUsuarios servicioUsuarios;
+		private readonly IRepositorioTransacciones repositorioTransacciones;
+		private readonly IRepositorioCuentas repositorioCuentas;
+
+		public TransaccionesController(IServicioUsuarios servicioUsuarios, IRepositorioTransacciones repositorioTransacciones, IRepositorioCuentas repositorioCuentas)
+		{
+			this.servicioUsuarios = servicioUsuarios;
+			this.repositorioTransacciones = repositorioTransacciones;
+			this.repositorioCuentas = repositorioCuentas;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> Crear()
+		{
+			var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+			var modelo = new TransaccionCreacionViewModel();
+			modelo.Cuentas = await ObtenerCuentas(usuarioId);
+
+			return View(modelo);
+		}
+
+		private async Task<IEnumerable<SelectListItem>> ObtenerCuentas(int usuarioId)
+		{
+			var cuentas = await repositorioCuentas.BuscarPorUsuario(usuarioId);
+			return cuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+		}
+	}
+}
