19275efe4e7497642af76e61d8cd7c456970d478
AlbertoJTD <albertojtd@outlook.com>
2023-06-01 19:34:45 -0600

    Add logic for weekly reports

---
+++ ManejoPresupuesto/Models/ReporteSemanalViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ManejoPresupuesto.Models
+{
+	public class ReporteSemanalViewModel
+	{
+        public decimal Ingresos => TransaccionesPorSemana.Sum(x => x.Ingresos);
+        public decimal Gatos => TransaccionesPorSemana.Sum(x => x.Gastos);
+        public decimal Total => Ingresos - Gatos;
+        public DateTime FechaReferencia { get; set; }
+        public IEnumerable<ResultadoObtenerPorSemana> TransaccionesPorSemana { get; set; }
+    }
+}
