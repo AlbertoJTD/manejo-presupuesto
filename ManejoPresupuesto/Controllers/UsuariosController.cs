51ff360f7abf223f916efd3803397ddae9827162
AlbertoJTD <albertojtd@outlook.com>
2023-06-02 14:36:03 -0600

    add sign up form

---
+++ ManejoPresupuesto/Controllers/UsuariosController.cs
@@ -0,0 +1,29 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+	public class UsuariosController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Registro()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Registro(RegistroViewModel modelo)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(modelo);
+			}
+
+			return RedirectToAction("Index", "Transacciones");
+		}
+	}
+}
